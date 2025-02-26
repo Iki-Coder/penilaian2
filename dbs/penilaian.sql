f3ab7aee1e6b588e16f1fdad6db3d28049181cf3
Iki-Coder <aalfiansyah887@gmail.com>
2025-02-26 15:20:06 +0700

    add dbs

--- dbs/penilaian.sql
+++ dbs/penilaian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 25 Feb 2025 pada 08.40
+-- Waktu pembuatan: 26 Feb 2025 pada 09.18
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.0.28
 
@@ -30,9 +30,21 @@ SET time_zone = "+00:00";
 CREATE TABLE `nilai` (
   `id` int(11) NOT NULL,
   `siswa_id` int(11) NOT NULL,
-  `mata_pelajaran` varchar(50) NOT NULL,
-  `nilai` int(11) NOT NULL
+  `mata_pelajaran` varchar(25) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `nilai_harian` int(11) NOT NULL,
+  `uh_1` int(11) NOT NULL,
+  `uh_2` int(11) NOT NULL,
+  `nilai_akhir_semester` int(11) NOT NULL,
+  `rata_rata` float DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `nilai`
+--
+
+INSERT INTO `nilai` (`id`, `siswa_id`, `mata_pelajaran`, `created_at`, `nilai_harian`, `uh_1`, `uh_2`, `nilai_akhir_semester`, `rata_rata`) VALUES
+(3, 6, 'PPS', '2025-02-26 07:40:53', 96, 78, 98, 67, 78.3);
 
 -- --------------------------------------------------------
 
@@ -60,6 +72,15 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(6, 'Rizki', '$2y$10$7w5wdwS78bdoVW4tjM3bkOAwlDZCseNxWMa8HiMhc4h9Hq0dT16ci', 'siswa'),
+(7, 'Siti', '$2y$10$JbWu415CJUTJOdHVPOG8I.aTH5Pq.kKyGZ7N0dh1ZLC7Sdz1cgXdS', 'guru'),
+(8, 'Lala', '$2y$10$c7pBv.K3EXfsABAP79k8N.9K2SmoAS3xENgwtN9tccfYwQte.Y1O6', 'siswa');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -68,7 +89,7 @@ CREATE TABLE `users` (
 --
 ALTER TABLE `nilai`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `siswa_id` (`siswa_id`);
+  ADD KEY `nilai_ibfk_1` (`siswa_id`);
 
 --
 -- Indeks untuk tabel `siswa`
@@ -92,7 +113,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `siswa`
@@ -104,7 +125,7 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -114,7 +135,7 @@ ALTER TABLE `users`
 -- Ketidakleluasaan untuk tabel `nilai`
 --
 ALTER TABLE `nilai`
-  ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`siswa_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
